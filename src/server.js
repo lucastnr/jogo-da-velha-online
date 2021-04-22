6f88c7332103b848b8bbc0c2bce3f6ef89de1c04
Lucas <tnrlucas@gmail.com>
2021-04-21 21:22:13 -0300

    make it better

---
+++ src/server.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+const staticPath = path.join(__dirname, "static");
+app.use(express.static(staticPath));
+app.set("views", path.join(staticPath));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.use("/", (req, res) => {
+    res.render("index.html");
+})
+
+let turn = 'X';
+let board = [];
+
+for (let i = 0; i < 9; i++) board[i] = " ";
+
+let connectedPlayers = [];
+
+io.on("connection", socket => {
+    handleConnect(socket);
+
+    socket.on("disconnect", () => 
+        handleDisconnect(socket)
+    )
+
+    socket.on("tileClick", (data) => 
+        handleTileClick(socket, data)
+    )
+
+    socket.on("gameReset", () => {
+        resetBoard(socket);
+    })
+})
+
+const handleConnect = (socket) => {
+    console.log(`Socket conectado ${socket.id}`)
+
+    const playerObject = {
+        id: socket.id,
+        value: getPlayerValue(),
+    }
+
+    socket.emit("playerObject", playerObject);
+    updateGame(socket);
+
+    connectedPlayers.push(playerObject);
+    logPlayers()
+}
+
+const updateGame = (socket) => {
+    socket.emit("gameUpdate", board);
+}
+
+const resetBoard = (socket) => {
+    for (let i = 0; i < 9; i++) board[i] = " ";
+    updateGame(socket);
+}
+
+const findPlayerIndex = (id) => {
+    return connectedPlayers.findIndex((el) => el.id == id);
+}
+
+const findPlayerObject = (id) => {
+    const playerIndex = findPlayerIndex(id);
+    if (playerIndex == -1) return undefined;
+    return connectedPlayers[playerIndex];
+}
+
+const handleDisconnect = (socket) => {
+    const id = socket.id;
+    console.log(`Socket desconectado ${id}`)
+    const playerIndex = findPlayerIndex(id);
+
+    playerIndex != 1 && connectedPlayers.shift(playerIndex);
+
+    logPlayers();
+}
+
+const logPlayers = () => {
+    console.log(`Jogadores conectados: ${connectedPlayers.length}\n`);
+}
+
+const handleReset = () => {
+    this.socket.emit("resetGame");
+}
+
+const handleTileClick = (socket, data) => {
+    const playerObject = findPlayerObject(socket.id);
+    const value = playerObject.value;
+    board[data.tilePos] = value;
+    updateGame(socket);
+}
+
+const getPlayerValue = () => {
+    if (connectedPlayers.length == 0) return "X";
+
+    if (connectedPlayers.length == 1) return "O";
+
+    return "INVALID";
+}
+
+server.listen(4000);
---
+++ src/static/front.js
@@ -0,0 +1,54 @@
+class Hash {
+    #tiles;
+    socket;
+    #player = "INVALID";
+    #modal;
+
+    constructor() {
+        this.#tiles = document.body.getElementsByClassName("tile");
+        this.#modal = document.getElementById("modal");
+        const reset = document.getElementById("reset");
+
+        this.socket = io("http://4f2a001209c6.ngrok.io/");
+
+        this.socket.on("gameUpdate", data => this.handleGameChange(data, this.socket))
+        this.socket.on("playerObject", data => this.handlePlayerObject(data))
+
+        reset.onclick = () => this.handleReset(this.socket);
+    }
+
+    handleGameChange(data, socket) {
+        for (let i = 0; i < 9; i++) {
+            this.#tiles[i].innerHTML = data[i];
+            this.#tiles[i].onclick = e => this.handleTileClick(e, socket)
+        }
+
+        this.#modal.className = "invisible"
+    }
+
+    handlePlayerObject(data) {
+        this.#player = data;
+    }
+
+    handleTileClick(e, socket) {
+        if (this.#player == "INVALID") return;
+
+        const tile = e.target;
+        const tilePos = tile.className.split(" ")[1];
+
+        if (tile.innerHTML != ' ') return;
+
+        this.#tiles[tilePos].innerHTML = this.#player.value;
+
+        socket.emit("tileClick", {
+            tilePos,
+            value: this.#player,
+        })
+    }
+
+    handleReset(socket) {
+        socket.emit("gameReset");
+    }
+}
+
+const _game = new Hash();
